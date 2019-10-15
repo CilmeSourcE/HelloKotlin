cf528e2cdc7be61c42c919e740718f61d1810258
cilmesource <46748859+CilmeSourcE@users.noreply.github.com>
2019-10-15 23:07:44 +0900

    Raw Stringを使ってみる

---
+++ src/main/RawStringMain.kt
@@ -0,0 +1,12 @@
+package main
+
+fun main(args: Array<String>){
+  var msg = """
+  kotlinと書いてコトリンと読む。
+  少しずつ勉強してみよう。
+
+  ちなみに、このようにダブルクオート3つで囲うことで、
+  複数行の文字を格納することができるのだ。
+ """"
+  println(msg)
+}
