7b2e9190e8a71eb0ed410fc2cd675b2737bf9d0b
cilmesource <46748859+CilmeSourcE@users.noreply.github.com>
2019-10-19 20:50:36 +0900

    コンパイル時定数の勉強
    Javaでいうstatic final的な使い方でよいのか...?
    うまく説明できないのが悔しいが、とりあえず書き方を覚えたということで次へ行きます。

--- src/main/ConstMain.kt
+++ src/main/ConstMain.kt
@@ -1,7 +1,6 @@
 package main
 
 fun main(args: List<String>) {
-
   // val命令にて変数を宣言することによって、定数化ができる
   val author = "cilmeSource"
 //	 author = "c11me"  // 再代入できない
@@ -16,5 +15,9 @@ fun main(args: List<String>) {
   // もちろん再代入はできない
   val ary = arrayOf(10, 20, 30)
   // ary = arrayOf(40, 50, 60)
-
 }
+  // constを付けてval命令をすることで、コンパイル時定数にできる
+  // Javaでいる static final的なものだと認識してます
+  // ただのvalとの違いは、、、直接定数へアクセスされることくらい？
+  // ※順に学びながら書いているので、あえて下に記述してます
+  const val str = ""
